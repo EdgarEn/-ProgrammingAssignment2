4e088b0e0f942083c1663f20b0effe25519737c2
Edgar Diaz <eediazh@unal.edu.co>
2020-11-27 21:35:54 -0500

    Second part

--- Assignment2.R
+++ Assignment2.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+x <- c(1,1,1,1)
+dim(x)<-c(2,2)
 
 makeCacheMatrix <- function(x = matrix()) {
-  
+m <- NULL
+set <- function(y) {
+  x <<- y
+  m <<- NULL
 }
-
-
-## Write a short comment describing this function
+get <- function() x
+setsolve <- function(solve) m <<- solve
+getsolve <- function() m
+list(set = set, get = get,
+     setsolve = setsolve,
+     getsolve = getsolve)
+}
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+m <- matrix(rnorm(16),4,4)
+makeCacheMatrix(m)
+cacheSolve(makeCacheMatrix(m))
